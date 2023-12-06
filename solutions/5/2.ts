eee53051a44da16293a2dcf6bb409c6cb50ec7a8
MrDiamondDog <84212701+MrDiamondDog@users.noreply.github.com>
2023-12-06 16:54:23 -0700

    Day 5 (I forgor to commit it yesterday)

--- solutions/5/2.ts
+++ solutions/5/2.ts
@@ -1,3 +1,114 @@
-import {getInput} from "../../utils/input";
+import { getInput } from "../../utils/input";
 
-const input = getInput(5).split("\n");
+type Item =
+    | "seed"
+    | "soil"
+    | "fertilizer"
+    | "water"
+    | "light"
+    | "temperature"
+    | "humidity"
+    | "location";
+const itemOrder: Item[] = [
+    "seed",
+    "soil",
+    "fertilizer",
+    "water",
+    "light",
+    "temperature",
+    "humidity",
+    "location"
+];
+
+interface Map {
+    from: Item;
+    to: Item;
+    sources: number[];
+    destinations: number[];
+    ranges: number[];
+}
+const maps: Map[] = [];
+
+function map(value: number, from: Item, to: Item): number {
+    const mapping = maps.find((map) => map.from == from && map.to == to);
+    if (!mapping) {
+        console.log(`no mapping found for ${from} -> ${to}`);
+        return -1;
+    }
+
+    for (let i = 0; i < mapping.sources.length; i++) {
+        const source = mapping.sources[i];
+        const destination = mapping.destinations[i];
+        const range = mapping.ranges[i];
+
+        if (
+            (value < source && value < destination) ||
+            (value > source + range && value > destination + range)
+        ) {
+            continue;
+        }
+
+        if (value >= source && value < source + range) {
+            return destination + value - source;
+        }
+    }
+
+    return value;
+}
+
+const input = getInput(5);
+const mapsList = input.split("\n\n");
+const seedsStr = mapsList.shift()!.split(" ");
+seedsStr.shift();
+const seeds: number[] = seedsStr.map((val) => parseInt(val));
+const seedPairs: { start: number, range: number; }[] = [];
+
+for (let i = 0; i < seeds.length - 1; i += 2) {
+    seedPairs.push({
+        start: seeds[i],
+        range: seeds[i + 1]
+    });
+}
+
+for (let i = 0; i < mapsList.length; i++) {
+    let mapStr = mapsList[i];
+    const map: Map = {
+        from: "seed",
+        to: "soil",
+        sources: [],
+        destinations: [],
+        ranges: []
+    };
+
+    const header = mapStr.split("\n")[0];
+
+    map.from = header.split("-to-")[0] as Item;
+    map.to = header.split("-to-")[1].replaceAll(" map:", "") as Item;
+
+    const mappings = mapStr.split("\n");
+    mappings.shift();
+
+    mappings.forEach((mapping) => {
+        const split = mapping.split(" ");
+
+        map.destinations.push(parseInt(split[0]));
+        map.sources.push(parseInt(split[1]));
+        map.ranges.push(parseInt(split[2]));
+    });
+
+    maps.push(map);
+}
+
+const locations: number[] = [];
+let total = 0;
+for (const seed of seedPairs) {
+
+}
+console.log(locations);
+
+let lowest = Number.MAX_VALUE;
+for (let i = 0; i < locations.length; i++) {
+    if (locations[i] < lowest) lowest = locations[i];
+}
+
+console.log("lowest:", lowest, "total seeds:", total);
